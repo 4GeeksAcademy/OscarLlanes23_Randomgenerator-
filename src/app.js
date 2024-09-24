10bb4603e70ccbbb619868ebd8c60769fb5ae8e9
Oscar Llanes <137604419+OscarLlanes23@users.noreply.github.com>
2024-09-24 00:15:20 +0000

    [Generador de Cartas entrega]

--- src/app.js
+++ src/app.js
@@ -6,6 +6,40 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let palos = ["♦", "♣", "♠", "♥"];
+
+  let valores = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+
+  let palosRandom = Math.floor(Math.random() * palos.length);
+
+  let valoresRandom = Math.floor(Math.random() * valores.length);
+
+  let paloArriba = document.getElementById("paloArriba");
+  let paloAbajo = document.getElementById("paloAbajo");
+  let numero = document.getElementById("valores");
+
+  paloArriba.innerHTML = palos[palosRandom];
+  paloAbajo.innerHTML = palos[palosRandom];
+  numero.innerHTML = valores[valoresRandom];
+
+  console.log(palos[palosRandom]);
+  if (palos[palosRandom] == "♥" || palos[palosRandom] == "♦") {
+    paloAbajo.style.color = "red";
+    paloArriba.style.color = "red";
+  }
 };
